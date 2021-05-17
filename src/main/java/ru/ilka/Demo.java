4bc046a442682c959ee6a835c6c0038421ba0197
Ilya Kisel <ikisel@workfusion.com>
2021-05-17 18:53:08 +0300

    init

---
+++ src/main/java/ru/ilka/Demo.java
@@ -0,0 +1,85 @@
+package ru.ilka;
+
+import java.io.IOException;
+
+public class Demo {
+    public static void main(String... args) throws IOException {
+
+        /* String */
+        //String text = "test";
+
+//        System.out.println("args:");
+//        for (String arg : args) {
+//            System.out.println(arg);
+//        }
+
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("Type your name");
+//        String name = reader.readLine();
+//        System.out.println("Hello " + name);
+
+
+        /* Арифметические операторы */
+
+//        int a = 10;
+//        int b = 5;
+//
+//        System.out.println(a + b);
+//        System.out.println(a - b);
+//        System.out.println(a / b);
+//        System.out.println(a % b);
+//
+//
+//        System.out.println(11%2)
+//   System.out.println(2 + 2 * 2);
+
+//        int i = 20;
+//        System.out.println(i + "21");
+
+        // Double example
+//        System.out.println(1 - 0.42);
+//        System.out.println(BigDecimal.valueOf(1).subtract(BigDecimal.valueOf(0.42)));
+
+
+        /* Операторы сравнения ==, !=, > , < , &&, ||, !*/
+        // System.out.println(!(5 == 6));
+
+
+        /* Оператор присваивания =, += */
+//        int i = 0;
+//        while (i < 10){
+//            i+=2;
+//            System.out.println(i+=2);
+//        }
+
+        /* Приведение типов */
+        // System.out.println(10 / (double) 0);
+
+        /* Унарные операторы
+Обычные арифметические операторы являются бинарными, так как имеют две части – правую и левую.
+ В Си подобных языках и в Java, в частности, имеются два унарных оператора,
+  то есть требующих только одного аргумента.
+  */
+//        int i = 1;
+//        System.out.println(i-- - (--i + i + 6));
+//        System.out.println("i = " + i);
+
+        /*
+
+         */
+
+        /*
+        method + return
+         */
+
+
+
+
+        /*
+        проверить чётное ли число
+        вывести N чисел из последовательности фибоначчи
+
+         */
+
+    }
+}
